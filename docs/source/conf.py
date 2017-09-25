6dc6484c1febb873f0c3e1b7ea9fb659af923609
Arno Simons <arno.simons@posteo.de>
2017-09-26 01:03:51 +0300

    a

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,11 +24,11 @@ THIS_YEAR = date.today().year
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('Projects/rdatools/rdatools/'))
+# sys.path.insert(0, os.path.abspath('Projects/rdatools/rdatools/'))
 
 # sys.path.insert(0, os.path.abspath('.'))
 # sys.path.insert(0, os.path.abspath('../'))
-# sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../../'))
 # sys.path.insert(1, "..") # from pyzotero
 
 
